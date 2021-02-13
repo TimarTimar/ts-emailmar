45b713d341f82202f5aa2b7c3db9da633f76be61
TimarTimar <tamasbelinszky@gmail.com>
2021-02-13 14:29:38 +0100

    fix spacing

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,9 @@
-if (proccess.env.NODE_ENV==='production'){
-    //production - return the prod set of keys - heroku set it...
-    module.exports=require('./prod');
-}else{
-    // development - return the dev keys
-    module.exports=require('./dev');
-}
+// keys.js - figure out what set of credentials to return
+if (process.env.NODE_ENV === 'production') {
+    // we are in production - return the prod set of keys
+    module.exports = require('./prod');
+  } else {
+    // we are in development - return the dev keys!!!
+    module.exports = require('./dev');
+  }
+  
