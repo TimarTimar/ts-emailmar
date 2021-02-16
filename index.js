da8ca4a32bb50affe578fea00704c21624531b4f
TimarTimar <tamasbelinszky@gmail.com>
2021-02-16 16:17:34 +0100

    root index sendFile, add /build to gitignore

--- index.js
+++ index.js
@@ -35,13 +35,13 @@ if (process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
 
     /*Express will serve up index html if doesn't recognize the file. It is decided by operation order.. Is there a file? Is there a routeHandler? Ok then handle the index.html*/
-    
-    const path=require('path');
-    app.get('*', (req,res)=>{
-        res.send(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
 
+    const path = require('path');
+    app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
+
 
 
 const PORT = process.env.PORT || 5000;
