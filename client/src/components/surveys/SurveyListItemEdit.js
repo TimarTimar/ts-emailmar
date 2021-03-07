e9c27d756599022d11d4614acc9526200c9dee22
TimarTimar <tamasbelinszky@gmail.com>
2021-03-07 14:21:51 +0100

    refactor SurveyListItemEdit.js to function component

--- client/src/components/surveys/SurveyListItemEdit.js
+++ client/src/components/surveys/SurveyListItemEdit.js
@@ -1,73 +1,67 @@
 import _ from "lodash";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { fetchSurvey } from "../../actions/index";
+import { useSelector, useDispatch } from "react-redux";
 import SurveyForm from "./SurveyForm";
-import { connect } from "react-redux";
 import axios from "axios";
 
-class SurveyListItemEdit extends React.Component {
-	state = { initaialValues: null };
-	componentDidMount() {
-		const run = async () => {
-			await this.props.fetchSurvey(this.props.match.params.surveyId);
-			const values = _.pick(this.props.surveys[0], [
-				"title",
-				"subject",
-				"body",
-			]);
-			const email2 = _.get(this.props.surveys[0], ["recipients", 0, "email"]);
-			const obj = { ...values, recipients: email2 };
-			this.setState({ initaialValues: obj });
-		};
-		run();
-	}
+const SurveyListItemEdit = (props) => {
+	const surveys = useSelector((state) => state.surveys);
+	console.log(surveys);
+	const formValues = useSelector((state) => state.form.surveyForm);
+	const dispatch = useDispatch();
 
-	async saveAsDraft() {
-		const surveyId = this.props.match.params.surveyId;
+	const [initaialValues, setInitialValues] = useState(null);
+
+	useEffect(() => {
+		dispatch(fetchSurvey(props.match.params.surveyId));
+	}, []);
+
+	useEffect(() => {
+		const values = _.pick(surveys[0], ["title", "subject", "body"]);
+		const email2 = _.get(surveys[0], ["recipients", 0, "email"]);
+		const obj = { ...values, recipients: email2 };
+		setInitialValues(obj);
+	}, [surveys]);
+
+	const saveAsDraft = async () => {
+		const surveyId = props.match.params.surveyId;
 		await axios.patch(`/api/edit_survey/${surveyId}`, {
-			...this.props.surveys[0],
-			...this.props.formValues.values,
+			...surveys[0],
+			...formValues.values,
 		});
 		window.location = "/surveys";
-	}
+	};
 
-	async sendEmail() {
-		const surveyId = this.props.match.params.surveyId;
+	const sendEmail = async () => {
+		const surveyId = props.match.params.surveyId;
 		await axios.patch(`/api/send_survey/${surveyId}`, {
-			...this.props.surveys[0],
-			...this.props.formValues.values,
+			...surveys[0],
+			...formValues.values,
 		});
-	}
+	};
 
-	render() {
-		return (
-			<div>
-				<SurveyForm
-					initialValues={this.state.initaialValues}
-					onSurveySubmit={() =>
-						this.sendEmail().then((window.location = "/surveys"))
-					}
-					submitbuttonname="Send"
-					submitbuttoniconname="email"
+	return (
+		<div>
+			<SurveyForm
+				initialValues={initaialValues}
+				onSurveySubmit={() => sendEmail().then((window.location = "/surveys"))}
+				submitbuttonname="Send"
+				submitbuttoniconname="email"
+			>
+				<button
+					className="purple btn-flat white-text right"
+					onClick={(e) => {
+						e.preventDefault();
+						e.stopPropagation();
+						saveAsDraft();
+					}}
 				>
-					<button
-						className="purple btn-flat white-text right"
-						onClick={(e) => {
-							e.preventDefault();
-							e.stopPropagation();
-							this.saveAsDraft();
-						}}
-					>
-						Save As Draft
-					</button>
-				</SurveyForm>
-			</div>
-		);
-	}
-}
-
-const mapStateToProps = (state) => {
-	return { surveys: state.surveys, formValues: state.form.surveyForm };
+					Save As Draft
+				</button>
+			</SurveyForm>
+		</div>
+	);
 };
 
-export default connect(mapStateToProps, { fetchSurvey })(SurveyListItemEdit);
+export default SurveyListItemEdit;
