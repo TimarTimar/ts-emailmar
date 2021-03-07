8c8c77cb286a54d6a75245944e2779114f4562a6
TimarTimar <tamasbelinszky@gmail.com>
2021-03-07 15:42:50 +0100

    refactor Payments.js to function component

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import StripeCheckout from 'react-stripe-checkout';
-import {connect} from 'react-redux';
-import * as actions from '../actions';
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+import { connect, useDispatch } from "react-redux";
+import { handleToken } from "../actions";
 
-class Payments extends React.Component{
-    render(){
-        return(
-            <StripeCheckout
-                name="Emailmar"
-                description="$5 for 5 email credits"
-                amount={500}
-                token={token=>this.props.handleToken(token)} // submit callback
-                stripeKey={process.env.REACT_APP_STRIPE_KEY}
-            >
-                <button className="btn">
-                    Add Credits
-                </button>
-            </StripeCheckout>
-        );
-    }
-}
+const Payments = (props) => {
+	const dispatch = useDispatch();
+	return (
+		<StripeCheckout
+			name="Emailmar"
+			description="$5 for 5 email credits"
+			amount={500}
+			token={(token) => dispatch(handleToken(token))} // submit callback
+			stripeKey={process.env.REACT_APP_STRIPE_KEY}
+		>
+			<button className="btn">Add Credits</button>
+		</StripeCheckout>
+	);
+};
 
-export default connect(null,actions)(Payments);
+export default Payments;
