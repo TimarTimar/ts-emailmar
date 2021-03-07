5359af20d96ac6c0647819b5e90cbce42b15e189
TimarTimar <tamasbelinszky@gmail.com>
2021-03-07 09:13:41 +0100

    refactor App.js to function component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import { connect } from "react-redux";
 import * as actions from "../actions";
@@ -9,29 +9,28 @@ import Dashboard from "../components/Dashboard";
 import SurveyNew from "../components/surveys/SurveyNew";
 import SurveyListItemEdit from "../components/surveys/SurveyListItemEdit";
 
-class App extends React.Component {
-	componentDidMount() {
+const App = (props) => {
+	/*componentDidMount() {
 		this.props.fetchUser();
-	}
+	}*/
 
-	render() {
-		return (
-			<div className="container">
-				<BrowserRouter>
-					<div className="container">
-						<Header />
-						<Route path="/" exact component={Landing} />
-						<Route path="/surveys" exact component={Dashboard} />
-						<Route path="/surveys/new" component={SurveyNew} />
-						<Route
-							path="/edit_survey/:surveyId"
-							component={SurveyListItemEdit}
-						/>
-					</div>
-				</BrowserRouter>
-			</div>
-		);
-	}
-}
+	useEffect(() => {
+		props.fetchUser();
+	}, []);
+
+	return (
+		<div className="container">
+			<BrowserRouter>
+				<div className="container">
+					<Header />
+					<Route path="/" exact component={Landing} />
+					<Route path="/surveys" exact component={Dashboard} />
+					<Route path="/surveys/new" component={SurveyNew} />
+					<Route path="/edit_survey/:surveyId" component={SurveyListItemEdit} />
+				</div>
+			</BrowserRouter>
+		</div>
+	);
+};
 
 export default connect(null, actions)(App);
