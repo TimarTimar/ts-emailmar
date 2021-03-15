307df6ad83b9463e956f0bfa9a32b93c0e1d54b7
TimarTimar <tamasbelinszky@gmail.com>
2021-03-15 19:04:03 +0100

    trying to migrate passport.js to ts

--- services/passport.js
+++ services/passport.js
@@ -4,6 +4,12 @@ const keys = require("../config/keys");
 
 import { User } from "../models/User";
 
+/* I had the following issues when i tried to migrate this file to ts:
+
+1. extend express user with my user model: https://github.com/DefinitelyTyped/DefinitelyTyped/commit/91c229dbdb653dbf0da91992f525905893cbeb91#r34805708-> TypeError: Unable to require file: models\User.ts
+
+2. Done is not a function: https://github.com/jaredhanson/passport/issues/421 */
+
 passport.serializeUser((user, done) => {
 	// reason1: we use user._id which is the database id, cause we could implement multiple strategy exclude google
 	// reason2: Google Auth separate concern, we use our own id from here, ty
@@ -27,7 +33,36 @@ passport.use(
 			callbackURL: "/auth/google/callback",
 			proxy: true,
 		},
-		async (accessToken, refreshToken, profile, done) => {
+		function (accessToken, refreshToken, profile, done) {
+			// Do stuff with the profile like add it to your DB.
+			User.findOne({
+				googleId: profile.id,
+			})
+				.then((dbUserRecord, err) => {
+					if (dbUserRecord) {
+						// This user was found in our database since they already logged in before.
+						done(null, dbUserRecord);
+					} else {
+						// This is a new user so lets create a new entry for them.
+						const newUser = new User({
+							googleId: profile.id,
+						});
+
+						// Here we save the new user to our database
+						newUser.save().then((newUser) => {
+							// When done is called, it sends us back to the callback URL defined on line 8
+							done(null, newUser);
+						});
+					}
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		}
+	)
+);
+
+/*async (accessToken, refreshToken, profile, done) => {
 			const existingUser = await User.findOne({ googleId: profile.id });
 
 			if (existingUser) {
@@ -36,6 +71,4 @@ passport.use(
 			const user = await new User({ googleId: profile.id }).save();
 			// user is what we get back from the db
 			done(null, user);
-		}
-	)
-);
+		} */
