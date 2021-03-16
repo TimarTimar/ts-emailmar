2ce5dcc0081f7db8ad2c87c57d703da14024cb8a
TimarTimar <tamasbelinszky@gmail.com>
2021-03-16 11:27:12 +0100

    setupProxy->tsx

--- client/src/setupProxy.ts
+++ client/src/setupProxy.ts
@@ -1,5 +1,7 @@
+import { Express } from "express";
+
 const { createProxyMiddleware } = require("http-proxy-middleware");
-module.exports = function (app) {
+module.exports = function (app:Express) {
   app.use(
     ["/api", "/auth/google"],
     createProxyMiddleware({
