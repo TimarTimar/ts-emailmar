9942ab7116c89f5f43318af812c07ab5728be2ec
TimarTimar <tamasbelinszky@gmail.com>
2021-03-19 10:57:59 +0100

    action creators-> ts --min

--- client/src/actions/index.ts
+++ client/src/actions/index.ts
@@ -1,19 +1,21 @@
 import axios from "axios";
+import { Dispatch } from "redux";
+import { Survey } from "../reducers/types";
 import { FETCH_USER, FETCH_SURVEYS, FETCH_SURVEY } from "./types";
 
-export const fetchUser = () => async (dispatch) => {
+export const fetchUser = () => async (dispatch:Dispatch) => {
 	const res = await axios.get("/api/current_user");
 
 	dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-export const handleToken = (token) => async (dispatch) => {
+export const handleToken = (token:any) => async (dispatch:Dispatch) => {
 	const res = await axios.post("/api/stripe", token);
 
 	dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-export const submitSurvey = (values, history) => async (dispatch) => {
+export const submitSurvey = (values:Survey, history:any) => async (dispatch:Dispatch) => {
 	const res = await axios.post("/api/surveys", values);
 
 	history.push("/surveys");
@@ -21,13 +23,13 @@ export const submitSurvey = (values, history) => async (dispatch) => {
 	dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-export const fetchSurveys = () => async (dispatch) => {
+export const fetchSurveys = () => async (dispatch:Dispatch) => {
 	const res = await axios.get("/api/surveys");
 
 	dispatch({ type: FETCH_SURVEYS, payload: res.data });
 };
 
-export const fetchSurvey = (surveyId) => async (dispatch) => {
+export const fetchSurvey = (surveyId:string) => async (dispatch:Dispatch) => {
 	const res = await axios.get(`/api/fetch_survey/${surveyId}`);
 
 	dispatch({ type: FETCH_SURVEY, payload: res.data });
