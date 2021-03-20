8fcb9aabae8bc418b0e2236a6e005485af28dd34
TimarTimar <tamasbelinszky@gmail.com>
2021-03-20 10:45:08 +0100

    client index->tsx --min

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -9,7 +9,7 @@ import reduxThunk from "redux-thunk";
 import App from "./components/App";
 import { rootReducer } from "./reducers";
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancer = (window && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 const store = createStore(
 	rootReducer,
