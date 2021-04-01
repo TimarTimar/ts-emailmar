e75b90ae696692d80bb7622ea7b6c7286dc5fbcb
TimarTimar <tamasbelinszky@gmail.com>
2021-04-01 10:12:04 +0200

    install Tailwind CSS

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,11 +5,13 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import reduxThunk from "redux-thunk";
+import "./index.css";
 
 import App from "./components/App";
 import { rootReducer } from "./reducers";
 
-const composeEnhancer = (window && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const composeEnhancer =
+	(window && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 const store = createStore(
 	rootReducer,
